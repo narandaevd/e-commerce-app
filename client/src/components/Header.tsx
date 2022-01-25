015a25b03a1e5de5389bb23cfe38fe99308bcc94
Narandaev Damir <narandaevd@mail.ru>
2022-01-25 03:05:41 +0300

    Styled return FC type, not his own...

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import {Box} from '@mui/material';
+import Material, {Box, styled} from '@mui/material';
+
+const SomeElement: React.FC = styled(Box)`
+    background-color: red;
+`
 
 export default function Header(): React.ReactElement {
   return (
     <Box>
         wefwef
+        <SomeElement>qwdqwdq</SomeElement>
     </Box> 
   );
 }
